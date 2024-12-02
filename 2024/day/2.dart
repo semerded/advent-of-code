8e0826d2b4d29eef603f1f32e7ae21b1692d82f3
Sem VB <sem.van.broekhoven@gmail.com>
2024-12-02 12:52:59 +0100

    first commit

---
+++ 2024/day/2.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+class RedNosedReportChecker {
+  String? filename;
+  List<String>? report;
+  int safeReports = 0;
+
+  RedNosedReportChecker(this.filename) {
+    report = File(this.filename!).readAsLinesSync();
+  }
+
+  int checkReports({bool tolerateSingleBadLevel = false}) {
+    safeReports = 0;
+    for (String line in this.report!) {
+      List<int> lineContent = line.split(" ").map(int.parse).toList();
+      if (!tolerateSingleBadLevel) {
+        if (_checkSingleReport(lineContent)) safeReports++;
+      } else {
+        if (_checkSingleReportsWithSingleTolerate(lineContent)) safeReports++;
+      }
+    }
+    return safeReports;
+  }
+
+  bool _checkSingleReport(List<int> lineContent) {
+    bool? ascending;
+    int? previousNumber;
+
+    for (int i = 0; i < lineContent.length; i++) {
+      int number = lineContent[i];
+      if (i == 0) {
+        previousNumber = number;
+        continue;
+      }
+
+      // check for direction breach
+      if (ascending == null) {
+        ascending = previousNumber! < number;
+      }
+      if ((previousNumber! < number && !ascending) || (previousNumber > number && ascending) || previousNumber == number) {
+        return false;
+      }
+
+      // check for jump breach
+      if ((previousNumber - number).abs() > 3) {
+        return false;
+      }
+
+      previousNumber = number;
+    }
+
+    return true;
+  }
+
+  bool _checkSingleReportsWithSingleTolerate(List<int> lineContent) {
+    for (int i = 0; i < lineContent.length; i++) {
+      List<int> singleTolerate = List<int>.from(lineContent);
+      singleTolerate.removeAt(i);
+      if (_checkSingleReport(singleTolerate)) return true;
+    }
+    return false;
+  }
+}
+
+void main() {
+  RedNosedReportChecker reportChecker = RedNosedReportChecker("day/2.txt");
+  print("Part 1: ${reportChecker.checkReports()}");
+  print("Part 2: ${reportChecker.checkReports(tolerateSingleBadLevel: true)}");
+}
